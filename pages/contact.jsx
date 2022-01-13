fadfcd16d42b92dd5824a763b47e91654ae30e2f
Radu <gith@radu.at>
2022-01-13 17:38:34 +0100

    Implement TODO's

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -22,8 +22,8 @@ function Contact() {
               labore anim adipisicing amet irure tempor.
             </p>
             <ul className="contact-links">
-              <li className="contact-item">Email: lutfhi@me.com</li>
-              <li className="contact-item">Phone: +6212344321007</li>
+              <li className="contact-item">Email: no-reply@mail.net</li>
+              <li className="contact-item">Phone: +435555566</li>
             </ul>
           </div>
         </div>
